cdc699651e39451fa9a861b6d46dfc803ac153bb
Lukasz <srednickilukasz@gmail.com>
2022-02-10 20:45:05 +0100

    Move files to Organizer dir

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ import os
 import shutil
 
 desktop = f'{os.environ["USERPROFILE"]}\Desktop'
+organizer = f'{desktop}/Organizer'
+
+if not os.path.isdir(organizer):
+    os.mkdir(organizer)
 
 for file in os.listdir(desktop):
     if '.' in file and file != 'desktop.ini':
         ext = os.path.splitext(file)[1]
-        dir = f'{desktop}/{ext}'
+        dir = f'{organizer}/{ext}'
         if not os.path.isdir(dir):
             os.mkdir(dir.lower())
         shutil.move(os.path.join(desktop, file), os.path.join(dir, file))
